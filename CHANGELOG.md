b1ad345ffb676624fc3db076efb1ffcaa741997e
Adam Matthiesen <amatthiesen@outlook.com>
2026-02-18 18:47:36 -0800

    chore: update package.json version, add changeset configuration, and create initial changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# css-js-gen
+
+## 1.0.1
+
+### Patch Changes
+
+- Tweaks module generation
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
     entry: ['./src/index.ts'],
     format: 'esm',
-    dts: true
+    dts: true,
+    publint: {
+        level: 'error', // 'warning' | 'error' | 'suggestion'
+    },
+    outExtensions: () => ({
+        js: `.js`,
+        dts: `.d.ts`,
+    })
 })
